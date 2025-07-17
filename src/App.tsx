cdfdba8ec3303824557c7d6ffdc139efd9622c7a
elijahmurimi40 <elijahmurimi40@gmail.com>
2025-07-17 21:27:56 +0300

    Fix speed nav appearance when width is above 1580px

--- src/App.tsx
+++ src/App.tsx
@@ -364,6 +364,7 @@ function App() {
         sideNavRefH!!.style.width = '200px';
         sideNavRefH!!.style.top = `${pfGridRef.current!!.offsetTop + 1}px`;
         sideNavRefH!!.style.left = `${Math.floor(reimaingWidth / 2) - 200 - 5}px`;
+        speedSideNavRef.current!!.style.left = `${Math.floor(reimaingWidth / 2)}px`;
         const h = pfGridHeight > 854 ? 854 : pfGridHeight - 11;
         sideNavRefH!!.style.height = `${h}px`;
       } else {
